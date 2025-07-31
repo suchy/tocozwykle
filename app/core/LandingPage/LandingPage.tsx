f536e0f6f5cc3bde5ac65d1858a7a1754099ad9c
Mateusz Suchoń <kontakt@suchego.pl>
2025-08-01 01:12:44 +0200

    Add fb link

--- app/core/LandingPage/LandingPage.tsx
+++ app/core/LandingPage/LandingPage.tsx
@@ -1,4 +1,4 @@
-import { ChevronDown, ShoppingCart } from '../components/icons.tsx';
+import { ChevronDown, Instagram, ShoppingCart } from '../components/icons.tsx';
 import { Layout, LayoutProps } from '../components/Layout.tsx';
 import { benefits, howItWorks, nav, products, questions } from './data.tsx';
 import { GridSection } from './GridSection.tsx';
@@ -202,54 +202,39 @@ export function LandingPage(props?: Props) {
                 </a>
               </li>
 
+              <li>
+                <a
+                  href='https://www.facebook.com/profile.php?id=61578888749223'
+                  title='To co zwykle na Facebook'
+                  class='text-white hover:text-gray-400'
+                >
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    width='24'
+                    height='24'
+                    viewBox='0 0 24 24'
+                    fill='none'
+                    stroke='currentColor'
+                    stroke-width='2'
+                    stroke-linecap='round'
+                    stroke-linejoin='round'
+                    class='lucide lucide-facebook-icon lucide-facebook'
+                  >
+                    <path d='M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z' />
+                  </svg>
+                </a>
+              </li>
               {
-                /* <li>
-              <a
-                href='/'
-                title='To co zwykle na Facebook'
-                class='text-white hover:text-gray-400'
-              >
-                <svg
-                  xmlns='http://www.w3.org/2000/svg'
-                  width='24'
-                  height='24'
-                  viewBox='0 0 24 24'
-                  fill='none'
-                  stroke='currentColor'
-                  stroke-width='2'
-                  stroke-linecap='round'
-                  stroke-linejoin='round'
-                  class='lucide lucide-facebook-icon lucide-facebook'
+                /*
+              <li>
+                <a
+                  href='/'
+                  title='To co zwykle na Instagram'
+                  class='text-white hover:text-gray-400'
                 >
-                  <path d='M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z' />
-                </svg>
-              </a>
-            </li>
-
-            <li>
-              <a
-                href='/'
-                title='To co zwykle na Instagram'
-                class='text-white hover:text-gray-400'
-              >
-                <svg
-                  xmlns='http://www.w3.org/2000/svg'
-                  width='24'
-                  height='24'
-                  viewBox='0 0 24 24'
-                  fill='none'
-                  stroke='currentColor'
-                  stroke-width='2'
-                  stroke-linecap='round'
-                  stroke-linejoin='round'
-                  class='lucide lucide-instagram-icon lucide-instagram'
-                >
-                  <rect width='20' height='20' x='2' y='2' rx='5' ry='5' />
-                  <path d='M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z' />
-                  <line x1='17.5' x2='17.51' y1='6.5' y2='6.5' />
-                </svg>
-              </a>
-            </li>*/
+                  <Instagram />
+                </a>
+              </li> */
               }
             </ul>
           </div>
--- app/core/components/icons.tsx
+++ app/core/components/icons.tsx
@@ -156,3 +156,17 @@ export const Handshake = (props: JSX.HTMLAttributes) => (
     <path d='m15 18-2-2' />
   </Svg>
 );
+
+export const Facebook = (props: JSX.HTMLAttributes) => (
+  <Svg {...props}>
+    <path d='M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z' />
+  </Svg>
+);
+
+export const Instagram = (props: JSX.HTMLAttributes) => (
+  <Svg {...props}>
+    <rect width='20' height='20' x='2' y='2' rx='5' ry='5' />
+    <path d='M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z' />
+    <line x1='17.5' x2='17.51' y1='6.5' y2='6.5' />
+  </Svg>
+);
